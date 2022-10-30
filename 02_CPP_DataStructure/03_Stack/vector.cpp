a096048e26975e7a3094245a50b338ed15a81ae8
kalyaniDhapodkar <dhapodkarkalyani@gmail.com>
2022-10-30 22:47:29 +0530

    30-Oct-2022

---
+++ 02_CPP_DataStructure/03_Stack/vector.cpp
@@ -0,0 +1,354 @@
+
+
+#pragma once
+
+#include <cstdarg>
+
+#include "mv_base_iterator.hpp"
+
+// Constant Literals
+
+#ifndef  	SUCCESS 
+#define 	SUCCESS  	0
+#endif
+
+#ifndef  	FAILURE 	
+#define  	FAILURE 	1
+#endif
+
+#ifndef  	DEFAULT_SIZE
+#define  	DEFAULT_SIZE  	32
+#endif
+
+template<typename T>
+class MVVector;
+
+typedef  	int ret_t;
+
+// Global Function Declaration
+template<typename T>
+std::ostream& operator<<(std::ostream& out, MVVector<T>& vec);
+
+template<typename T>
+std::ostream& operator<<(std::ostream& out, MVVector<T>*& vec);
+
+// Type definition 
+template<typename T>
+class MVVector
+{
+private:
+	T* m_data_array;
+	size_t 	m_no_of_elements;
+	size_t  m_capacity;
+
+	// Vector Helper Function
+	bool IsArray()const
+	{
+		// Code
+		if( m_data_array )
+			return(true);
+
+		return(false);
+	}
+
+public:
+	// Vector Interface Functions
+
+	// Default Constructor
+	MVVector()
+	{
+		// Code
+		this->m_capacity = DEFAULT_SIZE;
+		this->m_no_of_elements = 0;
+		this->m_data_array = NULL;
+
+		m_data_array = (T*)realloc( m_data_array, m_capacity * sizeof(T));
+
+		if(NULL == m_data_array)
+		{
+			std::cerr << "ERROR: Out Of Memory." << std::endl;
+			exit(FAILURE);
+		}
+
+		memset(m_data_array, 0, m_capacity * sizeof(T));
+	}
+
+	~MVVector()
+	{
+		if(m_data_array)
+		{
+			free(m_data_array);
+			m_data_array = NULL;
+		}
+	}
+
+	// Push Back
+	ret_t  	PushBack(T data)
+	{
+		// Code
+		if(!IsArray())
+		{
+			std::cerr << "ERROR: Vector Not Found." << std::endl;
+			return(FAILURE);
+		}
+
+		m_data_array[m_no_of_elements] = data;
+		m_no_of_elements++;
+
+		if(m_no_of_elements == m_capacity)
+		{
+			m_capacity = m_capacity * 2;
+			m_data_array = (T*)realloc(m_data_array, m_capacity * sizeof(T));
+		}
+	
+		return(SUCCESS);
+	}
+
+	ret_t operator+(T data)
+	{
+		// Code
+		return(PushBack(data));
+	}
+
+	// Variadic Push Back
+	ret_t  PushBackV(long no_of_values, ...)
+	{
+		// Code
+		va_list args;
+
+		va_start(args, no_of_value);
+		for(long le = 0; le < no_of_values; le++)
+		{
+			(*this) + va_arg(args, T);
+		}
+		va_end(args);
+
+		return(SUCCESS);
+	}
+
+	// Pop Back
+	T PopBack()
+	{
+		// Code
+		if(	!IsArray() ||
+			0 == m_no_of_elements)
+		{
+			std::cerr << "ERROR: Vector Not Found." << std::endl;
+			return((T)0);
+		}
+
+		m_no_of_elements--;
+
+		T to_return_data = m_data_array[m_no_of_elements];
+
+		if( m_no_of_elements <= m_capacity/2 &&
+			0 != m_no_of_elements)
+		{
+			m_capacity = m_capacity/2;
+			m_data_array = (T*)realloc(m_data_array, m_capacity * sizeof(T));
+		} 
+
+		return(to_return_data);
+	}
+
+
+	// Retrived Data
+	T DataAt(long index)const
+	{
+		// Code
+		if( !IsArray() ||
+			0 == m_no_of_elements)
+		{
+			std::cerr << "ERROR: Vector Not Found." << std::endl;
+			return((T)0);
+		}
+
+		if( index >= m_no_of_elements)
+		{
+			std::cerr << "ERROR: Index Out of Bound." << std::endl;
+			return((T)0);
+		}
+
+		return(m_data_array[index]);
+	}
+
+	// Getter Size
+	size_t Size()const
+	{
+		// Code
+		return(m_no_of_elements);
+	}
+
+	size_t Capacity()const
+	{
+		// Code
+		return(m_capacity);
+	}
+
+	T operator[](long index)const
+	{
+		// Code
+		return(DataAt(index));
+	}
+
+	class Iterator : public mv_base_iterator<T>
+	{
+	private:
+		T* m_data_ptr;
+
+	public:
+		Iterator(T* data_ptr)
+		{
+			// code
+			this->m_data_ptr = data_ptr;
+		}
+
+		~Iterator()
+		{
+			this->m_data_ptr = NULL;
+		}
+
+		// Pre Increment 
+		Iterator& operator++()
+		{
+			// Code
+			this->m_data_ptr++;
+			return(*this);
+		}
+
+		// Post Increment
+		Iterator operator++(int value)
+		{
+			Iterator itr = *this;
+			++*this;
+			return(itr);
+		}
+
+		// Pre Decrement
+		Iterator& operator--()
+		{
+			// Code 
+			this->m_data_ptr--;
+			return(*this);
+		}
+
+		// Post Decrement
+		Iterator operator--(int value)
+		{
+			Iterator itr = *this;
+			--*this;
+			return(itr);
+		}
+
+		Iterator operator+(int value)
+		{
+			Iterator itr = Iterator(m_data_ptr + value)
+
+			return(itr);
+		}
+
+		Iterator operator-(int value)
+		{
+			Iterator itr = Iterator(m_data_ptr - value);
+
+			return(itr);
+		}
+
+		Iterator& operator+=(int value)
+		{
+			// Code
+			this->m_data_ptr += value;
+
+			return(*this);
+		}
+
+		Iterator& operator-=(int value)
+		{
+			// Code
+			this->m_data_ptr -= value;
+
+			return(*this);
+		}
+
+		bool operator!=(Iterator& itr)
+		{
+			// Code
+			return(!((*this) == itr));
+		}
+
+		bool operator==(Iterator& itr)
+		{
+			// Code
+			return(this->m_data_ptr == itr.m_data_ptr);
+		}
+
+		T operator*()
+		{
+			// Code
+			return( *m_data_ptr);
+		}
+
+	};
+
+	Iterator begin()
+	{
+		// Code
+		return(Iterator(m_data_array));
+	}
+
+	Iterator end()
+	{
+		// Code
+		return(Iterator(&m_data_array[m_no_of_elements]));
+	}
+
+	void ForwardTraversal()
+	{
+		// Code
+		std::cout << (*this);
+	}
+
+	void ReverseTraversal()
+	{
+		// Code
+		std::cout << this;
+	}
+
+};
+
+template<typename T>
+std::ostream& operator<<(std::ostream& out, MVVector<T>& vec)
+{
+	// Code
+	for(auto itr = vec.begin(); itr != vec.end(); itr++)
+	{
+		std::cout << *itr << std::endl;
+	}
+
+	return(out);
+}
+
+template<typename T>
+std::ostream& operator<<(std::ostream& out, MVVector<T>*& vec)
+{
+	// Code
+	for(auto itr = vec.end(); itr != vec.begin(); ite--)
+	{
+		std::cout << *itr << std::endl;
+	}
+
+	return(out);
+}
+
+template<typename T>
+std::ostream& operator<<(std::ostream& out, MVVector<T>*&& vec)
+{
+	// Code
+	for(auto itr = vec->end()-1; itr != vec->begin()-1; itr--)
+	{
+		std::cout << *itr << std::endl;
+	}
+
+	return(out);
+}
+
